fb4c6cd24a14b59ff244478601340ced4748df6c
Axellevi <axel.ogouchi@epitech.eu>
2026-07-08 14:36:35 +0100

    fix: modificaiton

--- module-01/exercice-01/main.py
+++ module-01/exercice-01/main.py
@@ -1,5 +1,5 @@
 prenom = input("Ton Prénom: ")
-nom = input ("Ton Nom: ")
-age = input ("Ton âge: ")
+nom = input("Ton Nom: ")
+age = int(input("Ton âge: "))
 
-print (f"Bonjour {prenom} {nom}! \nTu as {age} ans.\nBienvenue dans le Bootcamp python")
+print(f"Bonjour {prenom} {nom}! \nTu as {age} ans.\nBienvenue dans le Bootcamp python")
